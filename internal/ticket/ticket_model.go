ac9c0a3c82d187b28c82c36b122d84642a7fe6b9
Adrián Constante <ad_con.reload@proton.me>
2025-07-11 09:44:39 -0700

    feat(ticket): agregar nuevos campos a la estructura de ticket
    Se añaden campos adicionales para mejorar la información de la sucursal,
    incluyendo nombre comercial, tienda, régimen y correos electrónicos.
    Esto permite un manejo más completo de los datos relacionados con
    las sucursales en el sistema.
    
    Signed-off-by: Adrián Constante <ad_con.reload@proton.me>

--- internal/ticket/ticket_model.go
+++ internal/ticket/ticket_model.go
@@ -75,18 +75,22 @@ type Ticket struct {
 	ClienteEmails         string `json:"cliente_emails"`
 
 	// Datos relacionales de Sucursal
-	SucursalRFC          string `json:"sucursal_rfc"`
-	SucursalNombre       string `json:"sucursal_nombre"`
-	SucursalRegimenClave string `json:"sucursal_regimen_clave"`
-	SucursalCalle        string `json:"sucursal_calle"`
-	SucursalNumero       string `json:"sucursal_numero"`
-	SucursalNumeroInt    string `json:"sucursal_numero_int"`
-	SucursalColonia      string `json:"sucursal_colonia"`
-	SucursalLocalidad    string `json:"sucursal_localidad"`
-	SucursalMunicipio    string `json:"sucursal_municipio"`
-	SucursalEstado       string `json:"sucursal_estado"`
-	SucursalCP           string `json:"sucursal_cp"`
-	SucursalPais         string `json:"sucursal_pais"`
+	SucursalRFC             string `json:"sucursal_rfc"`
+	SucursalNombre          string `json:"sucursal_nombre"`
+	SucursalNombreComercial string `json:"sucursal_nombre_comercial"`
+	SucursalTienda          string `json:"sucursal_tienda"`
+	SucursalRegimenClave    string `json:"sucursal_regimen_clave"`
+	SucursalRegimen         string `json:"sucursal_regimen"`
+	SucursalCalle           string `json:"sucursal_calle"`
+	SucursalNumero          string `json:"sucursal_numero"`
+	SucursalNumeroInt       string `json:"sucursal_numero_int"`
+	SucursalColonia         string `json:"sucursal_colonia"`
+	SucursalLocalidad       string `json:"sucursal_localidad"`
+	SucursalMunicipio       string `json:"sucursal_municipio"`
+	SucursalEstado          string `json:"sucursal_estado"`
+	SucursalCP              string `json:"sucursal_cp"`
+	SucursalPais            string `json:"sucursal_pais"`
+	SucursalEmail           string `json:"sucursal_email"`
 
 	// movimientos
 	Conceptos      []Concepto      `json:"conceptos"`
